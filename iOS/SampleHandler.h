de8a70a29e894f8da738c6a31c8d55f19c39ba5a
linjie321 <linjie321@users.noreply.github.com>
2022-04-22 15:15:07 +0800

    Add files via upload
    添加文件

---
+++ iOS/SampleHandler.h
@@ -0,0 +1,12 @@
+//
+//  SampleHandler.h
+//  BroadcastUploadExtension
+//
+//  Created by yalla758 on 2022/4/22.
+//
+
+#import <ReplayKit/ReplayKit.h>
+
+@interface SampleHandler : RPBroadcastSampleHandler
+
+@end
